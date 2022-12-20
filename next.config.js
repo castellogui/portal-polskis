82245fe8209029d57e7ca9e0fbdb3c05760e3bd9
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-20 00:48:17 -0300

    adicionando variaveis de ambiente

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    logins: ['emilynicole', 'guilhermecastello'],
+    senhas: ['mnb3698', '@Usuario10.'],
+    frases: [{ dica: "Nossa série favorita. Não escreva corretamente! <3", data: "2022-12-25", palavra: "Tinky Winky" }]
+  }
 }
 
 module.exports = nextConfig
